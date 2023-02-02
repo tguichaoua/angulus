0300cf37696c503ae0558463789963fb4c6ce3ec
Tristan Guichaoua <33934311+tguichaoua@users.noreply.github.com>
2023-02-02 13:20:23 +0100

    ♻️ rename `ToAngle::g` as `grad` (#10)

--- src/to_angle.rs
+++ src/to_angle.rs
@@ -15,9 +15,9 @@ pub trait ToAngle: Sized {
     /// Creates an unbounded angle with the value as turns.
     fn turns_unbounded(self) -> AngleUnbounded<Self>;
     /// Creates an angle with the value as gradians.
-    fn g(self) -> Angle<Self>;
+    fn grad(self) -> Angle<Self>;
     /// Creates an unbounded angle with the value as gradians.
-    fn g_unbounded(self) -> AngleUnbounded<Self>;
+    fn grad_unbounded(self) -> AngleUnbounded<Self>;
 }
 
 impl<F: Float> ToAngle for F {
@@ -45,11 +45,11 @@ impl<F: Float> ToAngle for F {
         AngleUnbounded::from_turns(self)
     }
 
-    fn g(self) -> Angle<Self> {
+    fn grad(self) -> Angle<Self> {
         Angle::from_gradians(self)
     }
 
-    fn g_unbounded(self) -> AngleUnbounded<Self> {
+    fn grad_unbounded(self) -> AngleUnbounded<Self> {
         AngleUnbounded::from_gradians(self)
     }
 }
